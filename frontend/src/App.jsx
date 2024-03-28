c39dd5cb82b7012226fde90c6166957652420c1e
ElCodMaler <140354428+ElCodMaler@users.noreply.github.com>
2024-03-28 10:11:09 -0400

    Revert "Botones descripcion"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,35 @@
+import ReactDOM from 'react-dom/client'
 import './App.css'
+import Home from './pages/Home'
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom'
+import Header from './layout/Header'
+import FooterE from './layout/FooterE'
+import Products from './pages/Products'
+import Descripcion from './pages/Descripcion'
+import AboutUs from './pages/AboutUs'
 
 function App() {
 
+  const { id } = useParams();
+
   return (
     <>
-      
+      <BrowserRouter>
+
+        <Header />
+
+        <Routes>
+
+          <Route path='/' element={<Home />} />
+          <Route path='/Productos' element={<Products />} />
+          <Route path="/descripcion/:id" element={<Descripcion />} />
+          <Route path='/Nosotros' element={<AboutUs />} />
+
+        </Routes>
+
+        <FooterE />
+
+      </BrowserRouter>
     </>
   )
 }
