a69e5b54da526422d9b3d64c26f6ef39b19a6d28
Erny23 <e.fava13@gmail.com>
2024-03-31 00:49:52 -0400

    cambios descripcion y listado

--- frontend/src/pages/Descripcion.jsx
+++ frontend/src/pages/Descripcion.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams, useNavigate } from 'react-router-dom'
 import { Carousel } from "flowbite-react"
 import { Tabs } from 'flowbite-react'
 import { AiFillTool, AiFillThunderbolt, AiOutlinePoweroff, AiOutlineUsb, AiOutlineArrowLeft } from 'react-icons/ai'
@@ -9,18 +9,28 @@ const productos = Object.keys(datos).map((producto) => datos[producto]);
 
 function Descripcion() {
 
-    const { id } = useParams();
+  const navigate = useNavigate();
 
-    const productoSeleccionado = productos.find((p) => p.id === id);
+  const { id } = useParams();
+  
+  const productoSeleccionado = productos.find((p) => p.id === id);
+  
+  const imagenesProducto = productoSeleccionado.imagenes;
+  
+  const puertosDeSalida = productoSeleccionado.puertosDeSalida;
+  
+  const listaPuertosSalida = puertosDeSalida.map((puerto) => {
+    
+    return {
+      tipo: puerto[0].split(":")[0].trim(),
+      descripcion: puerto[0].split(":")[1].trim(),
+    };
+  });
 
-    const puertosDeSalida = productoSeleccionado.puertosDeSalida;
-
-    const listaPuertosSalida = puertosDeSalida.map((puerto) => {
-      return {
-        tipo: puerto[0].split(":")[0].trim(),
-        descripcion: puerto[0].split(":")[1].trim(),
-      };
-    });
+  const handleBack = () => {
+    navigate('/Productos');
+    window.location.reload();
+  };
 
   return (
     <>
@@ -28,7 +38,7 @@ function Descripcion() {
 
             <section className='container mx-auto px-4 py-5'>
 
-              <Link to={'/Productos'}>
+              <Link to={'/Productos'} onClick={() => handleBack()}>
                 <div className='w-fit p-3 bg-white/30 rounded-md'>
                     <a style={{color:'white'}}><AiOutlineArrowLeft/></a>
                 </div>
@@ -39,18 +49,33 @@ function Descripcion() {
                 <div className='flex flex-col items-center'>
 
                 <div className="grid grid-cols-1 w-full items-center bg-white rounded-lg shadow md:grid-cols-2 md:w-auto shadow-xl">
-                  <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+                  <div className="h-72 sm:h-96 md:h-80 lg:h-96 xl:h-svh">
                     <Carousel>
-                      <img src={`../../public/${productoSeleccionado.imagen}`} alt="{productoSeleccionado.imagen}" />
-                      <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-                      <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+                      {imagenesProducto.map((imagenProducto, index) => (
+                        <div key={index}>
+                          <img src={`../../public/${imagenProducto}`} alt={imagenProducto.titulo} />
+                        </div>
+                      ))}
                     </Carousel>
+                  {/*imagenesProducto && (
+                    <Carousel>
+                      {imagenesProducto.map((imagenProducto, index) => (
+                        <Carousel.Item key={index}>
+                          <img src={imagenProducto} alt={imagenProducto.titulo} />
+                        </Carousel.Item>
+                      ))}
+                    </Carousel>
+                  )*/}
                   </div>
                   <div className="flex flex-col justify-between p-4 leading-normal md:px-12">
-    
+
                           <h5 className="mb-2 text-2xl font-bold tracking-tight md:pb-3">
-                            {productoSeleccionado.tituloTecnico}
+                            {productoSeleccionado.titulo}
                           </h5>
+    
+                          <h6 className="mb-2 text-xl font-semibold tracking-tight md:pb-3">
+                            {productoSeleccionado.tituloTecnico}
+                          </h6>
 
                           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                             {productoSeleccionado.descripcion}
