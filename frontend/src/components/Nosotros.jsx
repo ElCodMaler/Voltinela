e22cfb0ea9d3faf7969abf68714538a1c32e15ea
ElCodMaler <gilbertojeda30@gmail.com>
2024-04-01 14:57:05 -0400

    links voltinela

--- frontend/src/components/Nosotros.jsx
+++ frontend/src/components/Nosotros.jsx
@@ -1,34 +1,36 @@
+'use client';
+
+import { Button } from 'flowbite-react';
 import React from 'react'
-import { Button } from 'flowbite-react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 function Nosotros() {
   return (
     <>
       <br />
-      <div className='titulo container mx-auto w-max lg:py-8'>
-         <h3 className='text-lg text-white text-center font-bold px-4 py-1 rounded-full shadow-xl md:text-3xl md:py-2 lg:text-4xl lg:py-3' style={{backgroundColor:'#84cc16'}}>Nosotros</h3>
-      </div>
-      <br />
-      <Link to={'/Voltinela/Nosotros'}>
+        <Link to={'/Voltinela/Nosotros'}>
         <section id='aboutUs'>
-          <div className="bg-cover bg-center bg-no-repeat bg-[url('../../public/teclado.jpg')] bg-gray-600 bg-blend-multiply">
-              <div className="px-4 mx-auto max-w-screen-xl text-center py-12 md:py-16 lg:py-24 lg:px-20 xl:py-28">
-                  <h2 className="mb-4 text-2xl font-bold tracking-tight leading-none text-white md:text-4xl lg:text-5xl lg:pb-8">
-                    ¿Quienes somos?
-                  </h2>
-                  <p className='mb-1 text-base font-semibold text-white sm:px-16 lg:text-4xl'>
-                    Voltinela se enfoca en reforzar la seguridad energética y promover la autosuficiencia mediante 
-                    soluciones de energía renovable.
-                  </p>
-                  <div className="flex flex-wrap gap-3 justify-center mt-3 lg:pt-8">
-                    <Button className='focus:ring-0' size='xl' pill><h6 className='text-lg font-bold lg:text-4xl'>Misión</h6></Button>
-                    <Button className='focus:ring-0' size='xl' pill><h6 className='text-lg font-bold lg:text-4xl'>Visión</h6></Button>
+          <div className='titulo container mx-auto w-max shadow-xl'>
+              <h3 className='text-lg text-white text-center font-bold px-4 py-1 rounded-full md:text-3xl md:py-2 lg:text-4xl lg:py-3' style={{backgroundColor:'#84cc16'}}>
+                  Nosotros
+              </h3>
+          </div>
+          <br />
+          <div className="bg-cover bg-center bg-no-repeat bg-[url('teclado.jpg')] bg-gray-600 bg-blend-multiply" style={{backgroundImage:"url('teclado.jpg')"}}>
+              <div className="px-4 mx-auto max-w-screen-xl text-center py-12 md:py-16 lg:py-24 xl:py-28">
+                  <h2 className="mb-4 text-3xl font-bold tracking-tight leading-none text-white md:text-4xl lg:text-5xl">¿Quienes somos?</h2>
+                  <p className='mb-1 text-md font-semibold text-white lg:text-lg sm:px-16 lg:px-48'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi molestias eius ratione quas, obcaecati neque aperiam quaerat at.</p>
+                  <div className="flex flex-wrap gap-3 justify-center pt-3">
+                    <Button className='focus:ring-0' pill>Mision</Button>
+                    <Button className='focus:ring-0' pill>Vision</Button>
+                    <Button className='focus:ring-0' pill>Valores</Button>
                   </div>
               </div>
           </div>
+        
         </section>
       </Link>
+
     </>
   )
 }
