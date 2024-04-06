d62215449ffbea2362df79f0d753fc7c7713da88
ElCodMaler <gilbertojeda30@gmail.com>
2024-04-05 20:21:45 -0400

    cambio de referencias a las imagnes1

--- frontend/src/components/Listado.jsx
+++ frontend/src/components/Listado.jsx
@@ -27,13 +27,13 @@ function Listado(id) {
         if (selectedCategory === '') {
             return productos.map((producto) => (
                 <Card id={producto.id} className="m-2  bg-cover bg-no-repeat bg-center bg-white bg-blend-multiply shadow-xl" key={producto.id} onClick={() => handleSelectedProduct(producto.id)}>
-                    <img src={producto.imagen} alt={producto.titulo} />
+                    <img src={`../../public/${producto.imagen}`} alt={producto.titulo} />
                 </Card>
             ))
         } else {
             return productos.filter(producto => producto.categoria === selectedCategory).map((producto) => (
                 <Card id={producto.id} className="m-2  bg-cover bg-no-repeat bg-center bg-white bg-blend-multiply shadow-xl" key={producto.id} onClick={() => handleSelectedProduct(producto.id)}>
-                    <img src={producto.imagen} alt={producto.titulo} />
+                    <img src={`../../public/${producto.imagen}`} alt={producto.titulo} />
                 </Card>
             ))
         };
