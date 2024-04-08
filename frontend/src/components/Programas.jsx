d2519c19b096d2d87ce5d5c355cf428ba19e2ea3
erny <e.fava13@gmail.com>
2024-04-08 14:39:30 -0400

    creacion de paginas seccion programas

--- frontend/src/components/Programas.jsx
+++ frontend/src/components/Programas.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { Card } from 'flowbite-react';
 
 function Programas() {
@@ -12,29 +13,37 @@ function Programas() {
             <br />
             <div className='container mx-auto px-4' style={{width:'-webkit-fill-available'}}>
                 <div className='grid gab-2 grid-cols-2 space-x-3 h-32 md:h-48 lg:h-72 xl:h-96'>
-                    <Card className="carta border-0 h-full me-2 bg-no-repeat bg-cover bg-center bg-[url('respaldo.jpg')] bg-gray-400 bg-blend-multiply shadow-xl" style={{backgroundImage:"url('respaldo.jpg')"}}>
-                      <h5 className="text-center text-xl font-bold tracking-tight text-white md:text-3xl lg:text-4xl">
-                        Invita y Gana
-                      </h5>
-                    </Card>
-                    <Card className="carta border-0 h-full bg-no-repeat bg-cover bg-center bg-[url('respaldo.jpg')] bg-gray-400 bg-blend-multiply shadow-xl" style={{backgroundImage:"url('respaldo.jpg')"}}>
-                      <h5 className="text-center text-xl font-bold tracking-tight text-white md:text-3xl lg:text-4xl">
-                        Embajadores
-                      </h5>
-                    </Card>
+                    <Link to={'/Voltinela/InvitaYGana'}>
+                      <Card className="carta border-0 h-full me-2 bg-no-repeat bg-cover bg-center bg-[url('respaldo.jpg')] bg-gray-400 bg-blend-multiply shadow-xl" style={{backgroundImage:"url('respaldo.jpg')"}}>
+                        <h5 className="text-center text-xl font-bold tracking-tight text-white md:text-3xl lg:text-4xl">
+                          Invita y Gana
+                        </h5>
+                      </Card>
+                    </Link>
+                    <Link to={'/Voltinela/Embajadores'}>
+                      <Card className="carta border-0 h-full bg-no-repeat bg-cover bg-center bg-[url('respaldo.jpg')] bg-gray-400 bg-blend-multiply shadow-xl" style={{backgroundImage:"url('respaldo.jpg')"}}>
+                        <h5 className="text-center text-xl font-bold tracking-tight text-white md:text-3xl lg:text-4xl">
+                          Embajadores
+                        </h5>
+                      </Card>
+                    </Link>
                 </div>
                 <br />
                 <div className='grid gab-2 grid-cols-2 space-x-3 h-32 md:h-48 lg:h-72 xl:h-96'>
-                    <Card className="carta border-0 h-full me-2 bg-no-repeat bg-cover bg-center bg-[url('respaldo.jpg')] bg-gray-400 bg-blend-multiply shadow-xl" style={{backgroundImage:"url('respaldo.jpg')"}}>
-                      <h5 className="text-center text-xl font-bold tracking-tight text-white md:text-3xl lg:text-4xl">
-                        Instala+
-                      </h5>
-                    </Card>
-                    <Card className="carta border-0 h-full bg-no-repeat bg-cover bg-center bg-[url('respaldo.jpg')] bg-gray-400 bg-blend-multiply shadow-xl" style={{backgroundImage:"url('respaldo.jpg')"}}>
-                      <h5 className="text-center text-xl font-bold tracking-tight text-white md:text-3xl lg:text-4xl">
-                        Carreras
-                      </h5>
-                    </Card>
+                    <Link to={'/Voltinela/Instala+'}>
+                      <Card className="carta border-0 h-full me-2 bg-no-repeat bg-cover bg-center bg-[url('respaldo.jpg')] bg-gray-400 bg-blend-multiply shadow-xl" style={{backgroundImage:"url('respaldo.jpg')"}}>
+                        <h5 className="text-center text-xl font-bold tracking-tight text-white md:text-3xl lg:text-4xl">
+                          Instala+
+                        </h5>
+                      </Card>
+                    </Link>
+                    <Link to={'/Voltinela/Carreras'}>
+                      <Card className="carta border-0 h-full bg-no-repeat bg-cover bg-center bg-[url('respaldo.jpg')] bg-gray-400 bg-blend-multiply shadow-xl" style={{backgroundImage:"url('respaldo.jpg')"}}>
+                        <h5 className="text-center text-xl font-bold tracking-tight text-white md:text-3xl lg:text-4xl">
+                          Carreras
+                        </h5>
+                      </Card>
+                    </Link>
                 </div>
             </div>
         </section>
---
+++ frontend/src/pages/Instala.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InstalaCont from '../components/InstalaCont'
+
+function Instala() {
+  return (
+    <>
+      <InstalaCont />
+    </>
+  )
+}
+
+export default Instala
