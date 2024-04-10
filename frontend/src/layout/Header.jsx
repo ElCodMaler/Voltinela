8bbb434126183507c28753969030985785639458
erny <e.fava13@gmail.com>
2024-04-10 02:47:19 -0400

    cambios en disposicion de sidebar

--- frontend/src/layout/Header.jsx
+++ frontend/src/layout/Header.jsx
@@ -48,10 +48,10 @@ function Header({carrito}) {
             </nav>
           </Navbar>
           
-          <div id="drawer-navigation" className="fixed top-0 left-0 z-40 w-full h-screen p-4 overflow-y-auto transition-transform translate-x-full dark:bg-gray-800" tabindex="-1" aria-labelledby="drawer-navigation-label" style={{backgroundColor:'#00000082',backdropFilter: "blur(4px)"}}>
-              <img src="voltinela-logo-blanco.png" className="mr-3 h-6 sm:h-9" alt="Logo" />
+          <div id="drawer-navigation" className="fixed top-0 left-0 z-40 w-full h-screen p-8 overflow-y-auto transition-transform translate-x-full lg:p-16" tabindex="-1" aria-labelledby="drawer-navigation-label" style={{backgroundColor:'#00000082',backdropFilter: "blur(4px)"}}>
+              <img src="voltinela-logo-blanco.png" className="mr-3 h-6 sm:h-9 lg:h-16" alt="Logo" />
               <button type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 end-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" >
-                  <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                  <svg aria-hidden="true" className="w-8 h-8 lg:w-16 lg:h-16" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                   <span className="sr-only">Close menu</span>
               </button>
             <div className="py-4 overflow-y-auto"> 
