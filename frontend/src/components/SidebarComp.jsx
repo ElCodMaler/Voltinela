7f3f5aadc94a8240074109d75cc6da8adb19f196
erny <e.fava13@gmail.com>
2024-04-12 11:11:58 -0400

    Links sidebar

--- frontend/src/components/SidebarComp.jsx
+++ frontend/src/components/SidebarComp.jsx
@@ -13,14 +13,14 @@ function SidebarComp({carrito, handleClose}) {
   
   const handleSelect = (categoria) => {
     localStorage.setItem('categoria', categoria);
-    //navigate('/Voltinela/Productos');
+    navigate('/Voltinela/Productos');
     handleClose();
     return false;
   };
 
   const allProducts = () => {
     localStorage.setItem('categoria', '');
-    //navigate('/Voltinela/Productos');
+    navigate('/Voltinela/Productos');
     handleClose();
     return false;
   };
@@ -41,14 +41,14 @@ function SidebarComp({carrito, handleClose}) {
       <Sidebar className='w-100' aria-label="Sidebar with multi-level dropdown example">
             <Sidebar.Items>
               <Sidebar.ItemGroup>
-                <Link to={'/'} onClick={() => handleClose()}>
+                <Link to={'/Voltinela//'} onClick={() => handleClose()}>
                   <Sidebar.Item className='lg:text-4xl lg:py-6' icon={HiHome}>
                     Inicio
                   </Sidebar.Item>
                 </Link>
                 <Sidebar.Collapse className='lg:text-4xl lg:py-6' icon={HiDuplicate} label="Categorías">
                   {categorias.map((categoria) => (
-                    <Link to={'/Productos'}>
+                    <Link to={'/Voltinela//Productos'}>
                       <Sidebar.Item className='lg:text-4xl lg:py-6' key={categoria.id} onClick={() => handleSelect(categoria)}>
                         {categoria}
                       </Sidebar.Item>
@@ -58,12 +58,12 @@ function SidebarComp({carrito, handleClose}) {
                       Todos los Productos
                     </Sidebar.Item>
                 </Sidebar.Collapse>
-                <Link to={'/Nosotros'} onClick={() => handleClose()}>
+                <Link to={'/Voltinela//Nosotros'} onClick={() => handleClose()}>
                   <Sidebar.Item className='lg:text-4xl lg:py-6' href="#" icon={AiOutlineUsergroupAdd}>
                     Nosotros
                   </Sidebar.Item>
                 </Link>
-                <Link to={'/Carrito'} onClick={() => handleClose()}>
+                <Link to={'/Voltinela//Carrito'} onClick={() => handleClose()}>
                   <Sidebar.Item className='lg:text-4xl lg:py-6' href="#" icon={HiShoppingCart}>
                     Carrito
                   </Sidebar.Item>
